5437b6ddb7b348a2714596ec1e3ec8061310698c
Dheemanth1202 <dheemanthh74@gmail.com>
2025-07-31 20:07:18 +0530

    Add new file

---
+++ src/Pattern_Printing/SpecialPattern/OddNumberTriangle.java
@@ -0,0 +1,15 @@
+package Pattern_Printing.SpecialPattern;
+import java.util.Scanner;
+public class OddNumberTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= 2 * j -1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
