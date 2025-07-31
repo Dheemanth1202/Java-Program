1fd2bd66bd5642a155549b0fb09a2c6fb93fdcb8
Dheemanth1202 <dheemanthh74@gmail.com>
2025-07-31 20:36:43 +0530

    Add new file

--- src/Pattern_Printing/SpecialPattern/OddNumberTriangle.java
+++ src/Pattern_Printing/SpecialPattern/OddNumberTriangle.java
@@ -5,20 +5,24 @@ public class OddNumberTriangle {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n: ");
         int n = sc.nextInt();
-//        for(int i = 1; i <= n; i++){
-//            for (int j = 1; j <= i; j++) {
-//                System.out.print(2*j-1 + " ");
-//            }
-//            System.out.println();
-//        }
 
-//        for(int i = 1; i <= n; i++){
-//            for (int j = 1; j <= 2*i-1; j+=2) {
-//                System.out.print(j + " ");
-//            }
-//            System.out.println();
-//        }
+//        Method - 1
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++) {
+                System.out.print(2*j-1 + " ");
+            }
+            System.out.println();
+        }
 
+//        Method - 2
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= 2*i-1; j+=2) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+//        Mathematics method
         for(int i = 1; i <= n; i++){
             int a = 1;
             for (int j = 1; j <= 2*i-1; j+=2) {
